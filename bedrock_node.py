dd12d05dd69f4690b894dffd53f14c4146c1457a
jlonge4 <91354480+jlonge4@users.noreply.github.com>
2023-11-17 12:49:42 -0500

    Update bedrock_node.py

--- bedrock_node.py
+++ bedrock_node.py
@@ -5,16 +5,13 @@ import boto3
 from langchain.embeddings import BedrockEmbeddings
 from typing import TYPE_CHECKING, Any, Dict, Iterator, List, Optional, Union, Tuple
 
-bedrock = boto3.client('bedrock-runtime')
-embedding_model = BedrockEmbeddings(client=bedrock)
-
 # Custom pipeline node logic since Haystack doesnt currently support Bedrock as a retriever
-class BedrockRetriever(BaseComponent):
+class BedrockEmbeddingRetriever(BaseComponent):
     outgoing_edges = 1
 
-    def __init__(self, document_store):
-        self.client = bedrock
-        self.embedding_model = embedding_model
+    def __init__(self, document_store, bedrock_client):
+        self.client = bedrock_client
+        self.embedding_model = BedrockEmbeddings(client=self.client)
         self.document_store = document_store
 
     def run(self, documents: List[Document]) -> tuple[dict[str, list[Document]], str]:
@@ -35,80 +32,43 @@ class BedrockRetriever(BaseComponent):
         return output, "output_1"
 
     def run_batch(self, documents: List[Document]) -> tuple[dict[str, list[Document]], str]:
-
-        content = [d.content for d in documents]
-        metadata = [d.meta for d in documents]
-        embeddings = self.embedding_model.embed_documents(content)
-        np_embedded = np.array(embeddings, dtype=np.float32)
-
-        docs = []
-
-        for i in range(0, len(embeddings)):
-            docs.append(Document(content=content[i], meta=metadata[i], embedding=np_embedded[i]))
-
-        output = {
-            "documents": docs,
-        }
-        return output, "output_1"
+        pass    
     
 
-class BedrockRetrieverNode(BaseComponent):
+class BedrockContextRetriever(BaseComponent):
     outgoing_edges = 1
 
-    def __init__(self, document_store, top_k):
-        self.client = bedrock
-        self.embedding_model = embedding_model
+    def __init__(self, document_store, top_k, filters, bedrock_client):
+        self.client = bedrock_client
+        self.embedding_model = BedrockEmbeddings(client=self.client)
         self.document_store = document_store
-        # self.filters = filters
+        self.filters = filters
         self.top_k = top_k
 
     def run(self, query) -> tuple[dict[str, list[Document]], str]:
         document_store = self.document_store
+        filters = self.filters
         embedding_model = self.embedding_model
         embedded_q = np.array([embedding_model.embed_query(query)], dtype=np.float32)
-        # if filters is not None:
-        #     num_docs_to_check = 50
-        #     res = document_store.query_by_embedding(embedded_q, top_k=num_docs_to_check)
-        # else:
-        answers = self.document_store.query_by_embedding(embedded_q, top_k=3)
 
-        output = {
-            "answers": answers,
-        }
-        return output, "output_1"
-    #         # TODO Implement filters
-    #         doc_filter = {
-    #             "document_name": {
-    #                 "$eq": f"{filters}"
-    #             }
-    #         }
+        if filters is not None:
+            num_docs_to_check = 30
+            res = document_store.query_by_embedding(embedded_q, top_k=num_docs_to_check)
+        else:
+            res = document_store.query_by_embedding(embedded_q, top_k=self.top_k)
 
-    #         if filters is not None:
-    #             cont = []
-    #             st.write(f'Using {filters} filter...')
-    #             for r in res:
-    #                 if r.meta['document_name'] == filters:
-    #                     cont.append(r)
-    #             if len(cont) == 0:
-    #                 cont = [Document(content="No Context Found", meta={"document_name": "N/A", "score": 0, "page": "N/A", "sha1": "N/A"})]
-    #                 return cont
-    #             top_k_cont = cont[:3]
-    #             return top_k_cont
-    #         else:    
-    #             return res
-
-    def run_batch(self, query) -> tuple[dict[str, list[Document]], str]:
-        document_store = self.document_store
-        embedding_model = self.embedding_model
-        embedded_q = np.array([embedding_model.embed_query(query)], dtype=np.float32)
-        # if filters is not None:
-        #     num_docs_to_check = 50
-        #     res = document_store.query_by_embedding(embedded_q, top_k=num_docs_to_check)
-        # else:
-        res = self.document_store.query_by_embedding(embedded_q, top_k=3)
-        answers = []
-        for i in range(0, len(res)):
-            answers.append(Document(content=res[i]))
+        if filters is not None:
+            cont = []
+            for r in res:
+                if r.meta['document_name'] == filters:
+                    cont.append(r)
+            if len(cont) == 0:
+                cont = [Document(content="No Context Found", meta={"document_name": "N/A", "score": 0, "page": "N/A", "sha1": "N/A"})]
+                return cont
+            top_k_cont = cont[:self.top_k]
+            answers = top_k_cont
+        else:    
+            answers = res
 
         output = {
             "answers": answers,
